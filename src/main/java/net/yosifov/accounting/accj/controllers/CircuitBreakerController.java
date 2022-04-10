6e651eeebc68f4c3c7a2a4d3903222b2913a2216
Strashimir Yosifov <syosifov@gmail.com>
2022-04-10 13:40:28 +0300

    Bulkhead

--- src/main/java/net/yosifov/accounting/accj/controllers/CircuitBreakerController.java
+++ src/main/java/net/yosifov/accounting/accj/controllers/CircuitBreakerController.java
@@ -15,21 +15,21 @@ import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 @RestController
 public class CircuitBreakerController {
 
-    private Logger logger =
+    private final Logger logger =
             LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("/sample-api")
     //@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
     @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
     //@RateLimiter(name="default")
-    //@Bulkhead(name="sample-api")
+    @Bulkhead(name="sample-api")
     //10s => 10000 calls to the sample api
     public String sampleApi() {
         logger.info("Sample api call received");
-		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
-					String.class);
-		return forEntity.getBody();
-        //return "sample-api";
+//		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
+//					String.class);
+//		return forEntity.getBody();
+        return "sample-api";
     }
 
     public String hardcodedResponse(Exception ex) {
